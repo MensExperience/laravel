743bfb2dd3301c861df93f643cbf2272bc5c0e53
Mune <somuexp@gmail.com>
2020-10-19 22:08:13 +0900

    fix

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 class LineBotController extends Controller
 {
     public function index()
@@ -13,7 +16,18 @@ class LineBotController extends Controller
     }
     public function parrot(Request $request)
     {
+        // ログ
         Log::debug($request->header());
         Log::debug($request->input());
+
+        // LINEBotクラス(インスタンス化用）
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        // 署名を検証する機能（SDK）
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
     }
 }
