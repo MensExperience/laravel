46dbf723b3d0df7dc26b0b96a9fe295e79ae81ca
Mune <somuexp@gmail.com>
2020-10-25 21:25:12 +0900

    fix

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
 use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
@@ -14,20 +14,34 @@ class LineBotController extends Controller
     {
         return view('linebot.index');
     }
+
     public function parrot(Request $request)
     {
-        // ログ
         Log::debug($request->header());
         Log::debug($request->input());
 
-        // LINEBotクラス(インスタンス化用）
         $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
         $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
 
-        // 署名を検証する機能（SDK）
         $signature = $request->header('x-line-signature');
+
         if (!$lineBot->validateSignature($request->getContent(), $signature)) {
             abort(400, 'Invalid signature');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+        Log::debug($events);
+
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                Log::debug('Non text message has come');
+                continue;
+            }
+
+            $replyToken = $event->getReplyToken();
+            $replyText = $event->getText();
+            $lineBot->replyText($replyToken, $replyText);
         }
     }
 }
